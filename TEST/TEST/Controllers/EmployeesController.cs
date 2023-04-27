56ee43c953eefb5d197b7f90980ebeb5a76828f7
柏翔 <96458633+Bobo0808@users.noreply.github.com>
2023-04-27 17:16:51 +0800

    learn CRUD

---
+++ TEST/TEST/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TEST.Models;
+using TEST.Models.Domain;
+
+namespace TEST.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        //一種請求方式用於更新或刪除操作
+        [HttpPost]
+
+        //我需要這邊的解釋
+        public IActionResult add(AddEmployeeViewModel addEmployeeViewModel) 
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeViewModel.Name,
+                Email = addEmployeeViewModel.Email,
+                Salary = addEmployeeViewModel.Salary,
+                Department = addEmployeeViewModel.Department,
+                DateOfBirth = addEmployeeViewModel.DateOfBirth,
+                
+            
+            
+            };
+
+        }
+    }
+}
